2b86b6ceecf072701ef08d5d2e43767db7e21717
kirildz <kirildz@protonmail.com>
2020-09-03 11:29:24 +0300

    minor

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,17 +2,16 @@
 import commander from 'commander';
 import diff from '../index.js';
 
-const {Command} = commander;
+const { Command } = commander;
 
 const program = new Command();
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format')
-    .action(diff);
-
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action(diff);
 
 program.parse(process.argv);
 
--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 import path from 'path';
-import fs from "fs";
-
+import fs from 'fs';
 
 export default (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'));
+  const file2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'));
 
-    const file1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'));
-    const file2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'));
+  const concatedKeys = [...Object.keys(file1).sort(), ...Object.keys(file2).sort()];
 
-    const concatedKeys = [...Object.keys(file1).sort(), ...Object.keys(file2).sort()];
+  const uniqKeys = [...new Set(concatedKeys)];
 
-    const uniqKeys = [...new Set(concatedKeys)];
+  const diff = uniqKeys.map((el) => {
+    if (Object.prototype.hasOwnProperty.call(file1, el)
+        && Object.prototype.hasOwnProperty.call(file2, el)) {
+      return file1[el] === file2[el]
+        ? `   ${el}: ${file1[el]}\n`
+        : ` - ${el}: ${file1[el]}\n + ${el}: ${file2[el]}\n`;
+    } if (!Object.prototype.hasOwnProperty.call(file1, el)) {
+      return ` + ${el}: ${file2[el]}\n`;
+    }
+    return ` - ${el}: ${file1[el]}\n`;
+  });
 
-    const diff = uniqKeys.map(el => {
-        if (file1.hasOwnProperty(el) && file2.hasOwnProperty(el)) {
-            return  file1[el] === file2[el]
-                ? `   ${el}: ${file1[el]}\n`
-                : ` - ${el}: ${file1[el]}\n + ${el}: ${file2[el]}\n`
-        } else if (!file1.hasOwnProperty(el)) {
-            return ` + ${el}: ${file2[el]}\n`
-        } else {
-            return ` - ${el}: ${file1[el]}\n`
-        }
-    })
-
-    console.log(`{\n${diff.join((''))}}`)
-
-}
+  console.log(`{\n${diff.join((''))}}`);
+};
