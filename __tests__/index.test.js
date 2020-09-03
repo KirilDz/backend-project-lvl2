9142a367c79dfba8511c96a84dec0e414de069b6
kirildz <kirildz@protonmail.com>
2020-09-03 16:53:12 +0300

    minor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,15 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
+import {dirname} from 'path';
 import diff from '../index';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 test('Simple test', () => {
-  expect(diff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual('{\n'
+  expect(diff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual('{\n'
         + ' - follow: false\n'
         + '   host: hexlet.io\n'
         + ' - proxy: 123.234.53.22\n'
