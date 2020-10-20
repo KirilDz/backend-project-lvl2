e9d59c7a6006b3b83b7390f394c5fc6310519907
kirildz <kirildz@protonmail.com>
2020-10-20 18:10:42 +0300

    minor

--- train.js
+++ train.js
@@ -1,89 +1,91 @@
 import {isObject, getChildren} from './src/utils.js'
 import _ from 'lodash';
 
-const obj1 = {
-    "common": {
-        "setting1": "Value 1",
-        "setting2": 200,
-        "setting3": true,
-        "setting6": {
-            "key": "value",
-            "doge": {
-                "wow": "too much"
-            }
-        }
-    },
-    "group1": {
-        "baz": "bas",
-        "foo": "bar",
-        "nest": {
-            "key": "value"
-        }
-    },
-    "group2": {
-        "abc": 12345,
-        "deep": {
-            "id": 45
-        }
-    }
-}
-
-const obj2 = {
-    "common": {
-        "follow": false,
-        "setting1": "Value 1",
-        "setting3": {
-            "key": "value"
-        },
-        "setting4": "blah blah",
-        "setting5": {
-            "key5": "value5"
-        },
-        "setting6": {
-            "key": "value",
-            "ops": "vops",
-            "doge": {
-                "wow": "so much"
-            }
-        }
-    },
-
-    "group1": {
-        "foo": "bar",
-        "baz": "bars",
-        "nest": "str"
-    },
-
-    "group3": {
-        "fee": 100500,
-        "deep": {
-            "id": {
-                "number": 45
-            }
-        }
-    }
-}
-
-
 // const obj1 = {
-//     "host":"hexlet.io",
-//     "timeout":50,
-//     "proxy":"123.234.53.22",
-//     "follow":false,
-//     "next": {
-//         "number": 5
+//     "common": {
+//         "setting1": "Value 1",
+//         "setting2": 200,
+//         "setting3": true,
+//         "setting6": {
+//             "key": "value",
+//             "doge": {
+//                 "wow": "too much"
+//             }
+//         }
+//     },
+//     "group1": {
+//         "baz": "bas",
+//         "foo": "bar",
+//         "nest": {
+//             "key": "value"
+//         }
+//     },
+//     "group2": {
+//         "abc": 12345,
+//         "deep": {
+//             "id": 45
+//         }
 //     }
 // }
-//
 //
 // const obj2 = {
-//     "timeout":20,
-//     "verbose":true,
-//     "host":"hexlet.io",
-//     "next": {
-//         "rating": 4
+//     "common": {
+//         "follow": false,
+//         "setting1": "Value 1",
+//         "setting3": {
+//             "key": "value"
+//         },
+//         "setting4": "blah blah",
+//         "setting5": {
+//             "key5": "value5"
+//         },
+//         "setting6": {
+//             "key": "value",
+//             "ops": "vops",
+//             "doge": {
+//                 "wow": "so much"
+//             }
+//         }
+//     },
+//
+//     "group1": {
+//         "foo": "bar",
+//         "baz": "bars",
+//         "nest": "str"
+//     },
+//
+//     "group3": {
+//         "fee": 100500,
+//         "deep": {
+//             "id": {
+//                 "number": 45
+//             }
+//         }
 //     }
 // }
+
+
+const obj1 = {
+    "host":"hexlet.io",
+    "timeout":50,
+    "proxy":"123.234.53.22",
+    "follow":false,
+    "next": {
+        "number": 5
+    }
+}
+
+
+const obj2 = {
+    "timeout":20,
+    "verbose":true,
+    "host":{
+        name: 'hexlet.io'
+    },
+    "next": {
+        "rating": 4
+    }
+}
 
 const types = {
     sameValues: 0,
@@ -93,48 +95,34 @@ const types = {
     deleted: 4
 }
 
+const diffBuilder = (keyName, diff, value) => {
+    return {
+        keyName: keyName,
+        difference: diff,
+        value: value
+    }
+};
+
 const dfs = (o1, o2) => {
     const allKeys = [...new Set([...Object.keys(o1), ...Object.keys(o2)])].sort();
-    console.log(allKeys)
     const data = allKeys.map(el => {
         if (Object.prototype.hasOwnProperty.call(o1, el) && Object.prototype.hasOwnProperty.call(o2, el)) {
             if (o1[el] === o2[el]) {
-                return {
-                    keyName: el,
-                    difference: types.sameValues,
-                    value: o1[el]
-                };
+                return diffBuilder(el, types.sameValues, o1[el]);
             } else {
                 if (isObject(o1[el]) && isObject(o2[el])) {
                     const recursion = dfs(o1[el], o2[el]);
-                    return {
-                        keyName: el,
-                        difference: types.treeValues,
-                        value: recursion
-                    }
+                    return diffBuilder(el, types.treeValues, recursion);
                 } else {
-                    return {
-                        keyName: el,
-                        difference: types.changedValue,
-                        value: [o1[el], o2[el]]
-                    }
+                    return diffBuilder(el, types.changedValue, [o1[el], o2[el]]);
                 }
             }
         } else {
             if (Object.prototype.hasOwnProperty.call(o1, el)) {
-                return {
-                    keyName: el,
-                    difference: types.deleted,
-                    value: o1[el]
-                }
+                return diffBuilder(el, types.deleted, o1[el]);
             } else {
-                return {
-                    keyName: el,
-                    difference: types.added,
-                    value: o2[el]
-                }
+                return diffBuilder(el, types.added, o2[el]);
             }
-
         }
     });
     console.log(data)
@@ -143,82 +131,52 @@ const dfs = (o1, o2) => {
 
 const data = dfs(obj1, obj2);
 
-
-const parser = (data) => {
-    const string = data.reduce((acc, el) => {
-        if (el.difference === 0) {
-            acc += `\n   ${el.keyName}: ${el.value}`;
-        }
-        if (el.difference === 1) {
-            acc += `\n - ${el.keyName}: ${el.value[0]}\n + ${el.keyName}: ${el.value[1]}`
-        }
-        if (el.difference === 2) {
-            const recursion = parser(el.value);
-            acc += `\n   ${el.keyName}: {${recursion}\n}`
-        }
-        if (el.difference === 3) {
-            acc += `\n + ${el.keyName}: ${el.value}`
-        }
-        if (el.difference === 4) {
-            acc += `\n - ${el.keyName}: ${el.value}`
+const transformObj = (obj) => {
+    const entries = Object.entries(obj);
+    const str = entries.reduce((acc, el) => {
+        if (isObject(el[1])) {
+            const recursion = transformObj(el[1]);
+            acc += `  ${el[0]}: ${recursion}\n`;
+        } else {
+            acc += `  ${el[0]}: ${el[1]}\n`;
         }
         return acc;
     }, '');
-    console.log(`{${string}\n}`)
-    return string;
+    // console.log(`{\n${str}}`)
+    return `{\n${str}}`;
+};
+
+
+const parser = (data) => {
+    const string = data.reduce((acc, el) => {
+        switch (el.difference) {
+            case 0:
+                acc += `\n   ${el.keyName}: ${isObject(el.value) ? transformObj(el.value) : el.value}`;
+                break;
+            case 1:
+                acc += `\n - ${el.keyName}: ${isObject(el.value[0]) 
+                    ? transformObj(el.value[0]) 
+                    : el.value[0]}\n + ${el.keyName}: ${isObject(el.value[1]) ? transformObj(el.value[1]) : el.value[1] }`;
+                break;
+            case 2:
+                const recursion = parser(el.value);
+                acc += `\n   ${el.keyName}: {${recursion}\n}`;
+                break;
+            case 3:
+                acc += `\n + ${el.keyName}: ${isObject(el.value) ? transformObj(el.value) : el.value}`;
+                break;
+            case 4:
+                acc += `\n - ${el.keyName}: ${isObject(el.value) ? transformObj(el.value) : el.value}`;
+                break;
+        }
+        return acc;
+    }, '');
+    // console.log(`{${string}\n}`)
+    return `{${string}\n}`;
 }
 
-parser(data)
+
+console.log(parser(data))
 
 
-
-// export const dfs = (o1, o2) => {
-//     const uniqKeys = [...new Set([...Object.keys(o1), ...Object.keys(o2)])];
-//     console.log('This is the uniqKeys');
-//     console.log(uniqKeys)
-//     const data = uniqKeys.reduce((acc, el) => {
-//         if (isObject(o1[el]) && o2.hasOwnProperty(el)) {
-//             console.log(`this elements are: `)
-//             console.log(o1[el])
-//             console.log(o2[el])
-//             console.log('(=))))))))))))')
-//             const item = el;
-//             const recursion = dfs(o1[el], o2[el]);
-//             console.log(recursion)
-//             acc['equal']
-//                 ? acc.equal.push({[item]: recursion})
-//                 : acc['equal'] = [{[item]: recursion}];
-//         } else {
-//             if (o1[el] === o2[el]) {
-//                 console.log('This elements are equal')
-//                 const item = el;
-//                 acc['equal']
-//                     ? acc.equal.push({[item]: o1[el]})
-//                     : acc['equal'] = [{[item]: o1[el]}];
-//                 console.log(o1[el])
-//                 console.log(o2[el])
-//             } else if (Object.prototype.hasOwnProperty.call(o1, el) && !Object.prototype.hasOwnProperty.call(o2, el)) {
-//                 const item = el;
-//                 acc['minus']
-//                     ? acc.minus.push({[item]: o1[el]})
-//                     : acc['minus'] = [{[item]: o1[el]}];
-//                 console.log('First object has this prop but second doesn\'t');
-//                 console.log(el)
-//             } else if (!Object.prototype.hasOwnProperty.call(o1, el) && Object.prototype.hasOwnProperty.call(o2, el)) {
-//                 console.log('Second object has this prop but first doesn\'t');
-//                 const item = el;
-//                 acc['plus']
-//                     ? acc.minus.push({[item]: o2[el]})
-//                     : acc['plus'] = [{[item]: o2[el]}];
-//                 console.log(el)
-//             }
-//         }
-//         return acc;
-//     }, {});
-//
-//     return data;
-// }
-//
-//
-// console.log(dfs(obj1, obj2))
 
