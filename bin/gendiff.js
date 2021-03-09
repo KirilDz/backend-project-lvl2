7122ddbb213082fdad880ad36c96183b89aa1b63
kirildz <kirildz@protonmail.com>
2021-03-09 08:36:43 +0200

    update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import commander from 'commander';
-import fs from 'fs';
-import dfs from '../src/compareFiles.js';
+import dfs from '../index.js';
 
 commander
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.\n')
   .option('-f, --format <type>', 'output format')
   .arguments('<file1>, <file2>')
-  .action((file1, file2) => dfs(JSON.parse(fs.readFileSync(`${process.cwd()}/${file1}`)), JSON.parse(fs.readFileSync(`${process.cwd()}/${file2}`))));
+  .action(dfs);
 
 commander.parse(process.argv);
--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,38 +1,31 @@
-import { isObject } from "./utils.js";
+import { isObject } from './utils.js';
 
-export const treeDifferenceBuilder = (firstEl, secondEl) => {
-
+export default (firstDoc, secondDoc) => {
+  const treeDifferenceBuilder = (firstEl, secondEl) => {
     const mergeKeys = [...new Set([...Object.keys(firstEl), ...Object.keys(secondEl)])].sort();
 
     return mergeKeys.map((key) => {
+      if (isObject(firstEl[key]) && isObject(secondEl[key])) {
+        const children = treeDifferenceBuilder(firstEl[key], secondEl[key]);
 
-        if (isObject(firstEl[key]) && isObject(secondEl[key])) {
+        return { key, children };
+      }
 
-            const children = treeDifferenceBuilder(firstEl[key], secondEl[key]);
+      if (firstEl[key] === secondEl[key]) {
+        return { key, same: firstEl[key] };
+      }
 
-            return { key, children };
-        }
+      if (!Object.prototype.hasOwnProperty.call(firstEl, key)) {
+        return { key, added: secondEl[key] };
+      }
 
-        if (firstEl[key] === secondEl[key]) {
+      if (!Object.prototype.hasOwnProperty.call(secondEl, key)) {
+        return { key, removed: firstEl[key] };
+      }
 
-            return { key, same: firstEl[key] };
-        } else {
-
-            if (!firstEl.hasOwnProperty(key)) {
-
-                return { key, added: secondEl[key] };
-            }
-
-            if (!secondEl.hasOwnProperty(key)) {
-
-                return { key, removed: firstEl[key] };
-            } else {
-
-                return { key, updated: [firstEl[key], secondEl[key]] };
-            }
-
-        }
+      return { key, updated: [firstEl[key], secondEl[key]] };
     });
+  };
+
+  return treeDifferenceBuilder(firstDoc, secondDoc);
 };
-
-
