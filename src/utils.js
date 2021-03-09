82c29486074dce51886fa0eb6de0072ca2ef3ba5
kirildz <kirildz@protonmail.com>
2021-03-09 09:58:26 +0200

    update

--- src/utils.js
+++ src/utils.js
@@ -1,64 +1,20 @@
-const objectToString = (data, level) => {
-
-    const outsideLevel = level;
-
-    const temporary = {};
-
-    const reducer = (data, level = 1) => {
-
-        const keys = Object.keys(data);
-
-        for (const key of keys) {
-            temporary[key] = level ;
-        }
-
-        return keys.reduce((acc, el) => {
-
-            if (typeof data[el] === 'object') {
-
-                const children = reducer(data[el], level += 1);
-
-                acc += `\n${spaceMaker(temporary[el] + outsideLevel)}${el}: {\n${children}\n${spaceMaker(temporary[el] + outsideLevel)}}`;
-
-                return acc;
-            }
-
-            if (keys.length === 1) {
-                acc += `${spaceMaker(temporary[el] + outsideLevel)}${el}: ${data[el]}`;
-            }
-
-            if (keys.indexOf(el) === keys.length - 1 && keys.length !== 1) {
-                acc += `\n${spaceMaker(temporary[el] + outsideLevel)}${el}: ${data[el]}`;
-            }
-
-            return acc;
-        }, '');
-
-    }
-
-    console.log(reducer(data))
-
-    return temporary;
-}
-
 const spacesAmountCalculator = (level, spaces = 0, start = 1) => {
-    const spaceCoefficient = 4;
+  const spaceCoefficient = 4;
 
-    if (level < start) return spaces;
-    return spacesAmountCalculator(level, spaces + spaceCoefficient, start + 1);
+  if (level < start) return spaces;
+  return spacesAmountCalculator(level, spaces + spaceCoefficient, start + 1);
 };
 
 const spaceMaker = (level, isWithSign = false) => {
-    const minusSign = isWithSign ? 2 : 0;
-    const spacesAmount = spacesAmountCalculator(level) - minusSign;
+  const minusSign = isWithSign ? 2 : 0;
+  const spacesAmount = spacesAmountCalculator(level) - minusSign;
 
-    return ' '.repeat(spacesAmount);
+  return ' '.repeat(spacesAmount);
 };
 
-const isObject = data => typeof data === 'object' && data !== null;
+const isObject = (data) => typeof data === 'object' && data !== null && !Array.isArray(data);
 
 export {
-    isObject,
-    objectToString,
-    spaceMaker
+  isObject,
+  spaceMaker,
 };
