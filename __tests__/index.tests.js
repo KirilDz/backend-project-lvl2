78a670805e85d08e5590f034a5b38cbe8490a4f3
kirildz <kirildz@protonmail.com>
2021-03-18 20:59:33 +0200

    update

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -4,7 +4,7 @@ import { test } from '@jest/globals';
 import diff from '../index.js';
 import stylishExample from '../__fixtures__/stylishExample.js';
 import plainExample from '../__fixtures__/plainExample.js';
-import jsonExample from "../__fixtures__/jsonExample.js";
+import jsonExample from '../__fixtures__/jsonExample.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,11 +12,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
-    [diff(getFixturePath('file1.json'), getFixturePath('file2.json')), stylishExample],
-    [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml')), stylishExample],
-    [diff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain'), plainExample],
-    [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain'), plainExample],
-    [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json'), jsonExample]
-])('All formatters tests', (diff, expected) => {
-    expect(diff).toBe(expected);
+  [diff(getFixturePath('file1.json'), getFixturePath('file2.json')), stylishExample],
+  [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml')), stylishExample],
+  [diff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain'), plainExample],
+  [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain'), plainExample],
+  [diff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json'), jsonExample],
+])('All formatters tests', (func, expected) => {
+  expect(func).toBe(expected);
 });
--- src/parser.js
+++ src/parser.js
@@ -3,29 +3,23 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const readFile = filepath => path.resolve(filepath);
+const extractData = (filepath) => {
+  const data = fs.readFileSync(path.resolve(filepath));
+  const format = path.extname(filepath).slice(1);
+  return {
+    data,
+    format,
+  };
+};
 
-const adsf = (filepath) => {
-  const fullPath = readFile(filepath);
-  const file = fs.readFileSync(fullPath);
-  const data = JSON.parse(file, 'utf-8');
-  console.log(data);
-}
-
-console.log(adsf('__fixtures__/file1.json'))
-
-// export default (filepath1) => {
-//   const extensions = {
-//     json: (filePath) => JSON.parse(fs.readFileSync(path.resolve(filePath), 'utf-8')),
-//     yml: (filePath) => yaml.load(fs.readFileSync(path.resolve(filePath), 'utf-8')),
-//     ini: (filePath) => ini.parse(fs.readFileSync(path.resolve(filePath), 'utf-8')),
-//   };
-//
-//   const filesExtension = path.extname(filepath1).slice(1);
-//
-//   return Object.prototype.hasOwnProperty.call(extensions, filesExtension)
-//       ? extensions[filesExtension](filepath1)
-//       : null;
-// };
-
-
+export default (filepath) => {
+  const { data, format } = extractData(filepath);
+  switch (format) {
+    case 'json':
+      return JSON.parse(data, 'utf-8');
+    case 'yml':
+      return yaml.load(data, 'utf-8');
+    default:
+      return ini.parse(data, 'utf-8');
+  }
+};
