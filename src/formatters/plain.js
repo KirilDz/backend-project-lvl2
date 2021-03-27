2934d64b37a3c7694a34b599c5ce150b5a914c82
kirildz <kirildz@protonmail.com>
2021-03-27 11:07:48 +0200

    update

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { stringCreator } from '../utils.js';
 
 const valueDefinition = (value) => {
   if (_.isPlainObject(value)) {
@@ -13,35 +12,8 @@ const valueDefinition = (value) => {
   return `${value}`;
 };
 
-// const formatToPlain = (entity) => {
-//   const builder = (data, parents = []) => data.reduce((acc, el) => {
-//
-//     if (_.has(el, 'children')) {
-//       const newParents = parents.concat(el.key);
-//       const children = builder(el.children, newParents);
-//       acc += children;
-//     }
-//     if (el.type === 'added') {
-//       const valuesString = parents.length > 0 ? `${parents.join('.')}.${el.key}` : `${el.key}`;
-//       acc += `\nProperty '${valuesString}' was added with value: ${valueDefinition(el.value)}`;
-//     }
-//     if (el.type === 'removed') {
-//       const valuesString = parents.length > 0 ? `${parents.join('.')}.${el.key}` : `${el.key}`;
-//       acc += `\nProperty '${valuesString}' was removed`;
-//     }
-//     if (el.type === 'updated') {
-//       const valuesString = parents.length > 0 ? `${parents.join('.')}.${el.key}` : `${el.key}`;
-//       acc += `\nProperty '${valuesString}' was updated. From ${valueDefinition(el.value1)} to ${valueDefinition(el.value2)}`;
-//     }
-//
-//     return acc;
-//   }, '');
-//   return `${builder(entity)}\n`;
-// };
-
 const formatToPlain = (entity) => {
-  const builder = (data, parents = []) => data.map((el) => {
-
+  const builder = (data, parents = []) => data.flatMap((el) => {
     if (_.has(el, 'children')) {
       const newParents = parents.concat(el.key);
       return builder(el.children, newParents);
@@ -52,19 +24,17 @@ const formatToPlain = (entity) => {
     }
     if (el.type === 'removed') {
       const valuesString = parents.length > 0 ? `${parents.join('.')}.${el.key}` : `${el.key}`;
-      return`\nProperty '${valuesString}' was removed`;
+      return `\nProperty '${valuesString}' was removed`;
     }
     if (el.type === 'updated') {
       const valuesString = parents.length > 0 ? `${parents.join('.')}.${el.key}` : `${el.key}`;
       return `\nProperty '${valuesString}' was updated. From ${valueDefinition(el.value1)} to ${valueDefinition(el.value2)}`;
     }
 
-    return '+';
+    return '';
   });
 
-  console.log(builder(entity))
-
-  return builder(entity).join(' ');
+  return builder(entity).join('');
 };
 
 export default formatToPlain;
